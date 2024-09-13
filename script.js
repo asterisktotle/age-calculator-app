8cd667612f34e60747396121586caa487805eaab
asterisktotle <barandajander.gmail.com>
2024-09-14 00:05:35 +0800

    date calculator day js

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+// SELECTOR
+const today = dayjs();
 const submitBtn = document.querySelector('.js-button');
-submitBtn.addEventListener('click', () => console.log('Button clicked'));
+const userInput = document.querySelectorAll('.userInput');
+
+// FUNCTIONS
+submitBtn.addEventListener('click', () => {
+	console.log('Button clicked');
+	let dateDigit = [];
+
+	userInput.forEach((digit) => {
+		dateDigit.push(digit.value);
+	});
+
+	const userYear = Number(dateDigit[2]);
+	const userMonth = Number(dateDigit[1]);
+	const userDay = Number(dateDigit[0]);
+
+	const userDate = dayjs(`${userYear}-${userMonth}-${userDay}`);
+
+	//years difference
+	const year = today.diff(userDate, 'year');
+	//month difference
+	const month = today.subtract(year, 'year').diff(userDate, 'month');
+	//day difference
+	const day = today
+		.subtract(year, 'year')
+		.subtract(month, 'month')
+		.diff(userDate, 'day');
+
+	console.log(year);
+	console.log(month);
+	console.log(day);
+});
